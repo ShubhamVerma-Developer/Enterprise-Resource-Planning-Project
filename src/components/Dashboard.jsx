f1a9786101aabf4e1d68d87a633a8d84f86468e7
ShubhamVermaTheDeveloper <shubham25122001@gmail.com>
2024-06-02 15:55:07 +0530

    desing layout done

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom';
 
 function Dashboard() {
   return (
-    <div className="bg-white rounded-lg shadow-md p-6">
-      <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+    <>
+    <div className='container'>
+    <div className="relative overflow-hidden bg-gradient-to-b from-blue-400 to-blue-600 rounded-lg shadow-md p-6">
+      <h1 className="text-3xl font-bold text-white mb-4">Dashboard</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div className="bg-gray-100 p-4 rounded-md">
           <p className="text-lg font-semibold">Total Products</p>
@@ -17,6 +19,8 @@ function Dashboard() {
         </div>
       </div>
     </div>
+    </div>
+    </>
   );
 }
 
